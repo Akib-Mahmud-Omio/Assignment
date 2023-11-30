7868b1d647c9d3a8215bcf97f8a94ea920b24250
DumbWaysToCode <assassinscreed.telenor@gmail.com>
2023-11-30 22:52:41 +0600

    second com

--- lib/main.dart
+++ lib/main.dart
@@ -12,3 +12,34 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class BookListPage extends StatelessWidget {
+  const BookListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Book List'),
+      ),
+      body: ListView(
+        children: const [
+          BookCard(
+            'Chemistry',
+            'Raymond Chang',
+            '4.8',
+            'https://chemistry.com.pk/wp-content/uploads/2021/03/Chemistry-13e-by-Raymond-Chang-and-Jason-Overby.jpg',
+            'This is a chemistry book for university students.There are 1006 pages in this book.',
+          ),
+          BookCard(
+            'Introductory Circuit Analysis',
+            'Robert Boylestad',
+            '4.6',
+            'https://static-01.daraz.com.bd/p/6ecb933f5fd7a8bc1064d7b4cf0e7066.jpg',
+            'This is an introductory circuit analysis book for electrical engineering.There are 1209 pages in this book.',
+          ),
+        ],
+      ),
+    );
+  }
+}
